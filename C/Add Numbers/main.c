dfe614ee9405dfb8b416c9d14029baafd6f5a6fd
Pasindu-Neranjana-Dasanayake <42902285+Pasindu-Neranjana-Dasanayake@users.noreply.github.com>
2018-10-29 12:49:34 +0530

    I added C program

---
+++ C/Add Numbers/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int no1,no2;
+
+    printf("Enter Two Numbers -->");
+    scanf("%d %d",&no1,&no2);
+
+    printf("\nThe Addition of entered Numbers are %d\n",(no1+no2));
+
+    return 0;
+}
